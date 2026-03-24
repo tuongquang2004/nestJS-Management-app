b829172f5740d56babbfb748dea5e6ca5885940c
Quang <tuong-quang.nguyen.etu@univ-lille.fr>
2026-03-24 10:39:25 +0700

    fix: HTTP Exception catch

--- src/http-exception.filter.ts
+++ src/http-exception.filter.ts
@@ -12,8 +12,12 @@ export class HttpExceptionsFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
+    console.error('Original Error:', exception);
     const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
 
     response.status(status).json({
       Time: new Date().toISOString(),
