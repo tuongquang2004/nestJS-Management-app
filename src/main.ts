4ab5b2d802d6a92007962e19e0a267c4e358a3ff
Quang <tuong-quang.nguyen.etu@univ-lille.fr>
2026-03-24 11:04:51 +0700

    fix: Add validationPipe to validate data of creating and patching posts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionsFilter } from './http-exception.filter';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionsFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,4 +1,17 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreatePostDto {
+  @IsString({ message: 'Title has to be a string' })
+  @IsNotEmpty({ message: 'Title is required' })
+  @MaxLength(150, {
+    message: 'Title is too long, maximum length is 150 characters',
+  })
   title: string;
+
+  @IsString({ message: 'Content must be a string' })
+  @IsNotEmpty({ message: 'Content is required' })
+  @MinLength(10, {
+    message: 'Content is too short, minimum length is 10 characters',
+  })
   content: string;
 }
