15b6b7678982d8a5336aae06fdcfd9d8f05ccd09
Quang <tuong-quang.nguyen.etu@univ-lille.fr>
2026-03-25 16:21:28 +0700

    fix: add more on user's dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString({ message: 'Username must be a string' })
+  @IsNotEmpty({ message: 'Username is required' })
   username: string;
+
+  @IsString({ message: 'Password must be a string' })
+  @MinLength(2, { message: 'Password must be at least 2 characters long' })
   password: string;
+
+  @IsEmail({}, { message: 'Email is not valid' })
+  @IsNotEmpty({ message: 'Email is required' })
   email: string;
 }
