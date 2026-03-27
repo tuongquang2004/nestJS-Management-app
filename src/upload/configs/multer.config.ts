e57212504829f932bbf4768880b622edfae02112
Quang <tuong-quang.nguyen.etu@univ-lille.fr>
2026-03-27 14:13:20 +0700

    fix: Add validation for type and size of the uploading file

--- src/upload/configs/multer.config.ts
+++ src/upload/configs/multer.config.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common/exceptions/bad-request.exception';
 import { diskStorage } from 'multer';
 import * as path from 'path';
 import { v4 as uuidv4 } from 'uuid';
@@ -9,11 +10,20 @@ const customFileName = (req: any, file: Express.Multer.File, cb: any) => {
   cb(null, `${filename}${extension}`);
 };
 
+const fileFilter = (req: any, file: Express.Multer.File, cb: any) => {
+  if (!file.mimetype.match(/^image\/(jpg|jpeg|png)$/)) {
+    return cb(new BadRequestException('only accept jpg, jpeg, png!'), false);
+  }
+  cb(null, true);
+};
+
 export const avatarStorage = {
   storage: diskStorage({
     destination: './uploads/avatars',
     filename: customFileName,
   }),
+  fileFilter: fileFilter,
+  limits: { fileSize: 1024 * 1024 * 5 },
 };
 
 export const postImageStorage = {
@@ -21,4 +31,6 @@ export const postImageStorage = {
     destination: './uploads/post-images',
     filename: customFileName,
   }),
+  fileFilter: fileFilter,
+  limits: { fileSize: 1024 * 1024 * 5 },
 };
