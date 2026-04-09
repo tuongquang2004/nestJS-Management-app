741a00d88a80423ab20837f783eb06ed8073ba66
Quang <tuong-quang.nguyen.etu@univ-lille.fr>
2026-04-09 09:00:29 +0700

    fix: (exception filter) use Logger instead of console log

--- src/http-exception.filter.ts
+++ src/http-exception.filter.ts
@@ -4,20 +4,30 @@ import {
   ArgumentsHost,
   HttpException,
   HttpStatus,
+  Logger,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Catch()
 export class HttpExceptionsFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  private readonly logger = new Logger(HttpExceptionsFilter.name);
+
+  catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    console.error('Original Error:', exception);
     const request = ctx.getRequest<Request>();
+
     const status =
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const logMessage = `[${request.method}] ${request.url} - Status: ${status}`;
+    let errorDetail = exception;
+    if (exception instanceof Error) {
+      errorDetail = exception.stack;
+    }
+    this.logger.error(logMessage, errorDetail);
 
     response.status(status).json({
       Time: new Date().toISOString(),
