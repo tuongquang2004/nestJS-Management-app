e863037c0b83e5e95c51ab873b8b196d97d0a360
Quang <tuong-quang.nguyen.etu@univ-lille.fr>
2026-04-20 11:34:45 +0700

    challenges (Swagger): users.controller, update-user.dto, user-response.dto, pagination.dto

--- src/users/dto/pagination.dto.ts
+++ src/users/dto/pagination.dto.ts
@@ -1,13 +1,25 @@
+import { ApiPropertyOptional } from '@nestjs/swagger/dist/decorators/api-property.decorator';
 import { Transform } from 'class-transformer';
 import { IsNumber, IsOptional, IsPositive, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiPropertyOptional({
+    description:
+      'The number of items to skip before starting to collect the result set',
+    example: 0,
+    minimum: 0,
+  })
   @IsOptional()
   @Transform(({ value }) => (value ? Number(value) : undefined))
   @IsNumber()
   @Min(0)
   skip?: number;
 
+  @ApiPropertyOptional({
+    description: 'The number of items to return',
+    example: 10,
+    minimum: 1,
+  })
   @IsOptional()
   @Transform(({ value }) => (value ? Number(value) : undefined))
   @IsNumber()
