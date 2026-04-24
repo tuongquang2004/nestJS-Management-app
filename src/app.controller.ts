6473b178901b17597776ae2179760f67f0250388
Quang <tuong-quang.nguyen.etu@univ-lille.fr>
2026-04-24 10:21:01 +0700

    challenges (Swagger): app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('Home')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'App Home Get',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK, // 200
+    description: 'The server is running normally.',
+    schema: {
+      type: 'string',
+      example: 'Hello World!',
+    },
+  })
   getHello(): string {
     return this.appService.getHello();
   }
